9163a3735c7e5bdab0ab9db13491f8d35493f185
Cleophile <16307130090@fudan.edu.cn>
2019-08-19 14:03:56 +0800

    Bulk Driver

--- test_driver.sh
+++ test_driver.sh
@@ -1,7 +1,7 @@
 count=60;
 while [ $count -le 119 ];
 do
-  $(printf "/home/graviti/WorkingMain/my_scalp/test %06d_10.png result_$count" $count);
+  $(printf "/home/graviti/WorkingMain/my_scalp/test ./%06d_10.png /home/graviti/下载/scalp_result/result_%06d.jpg /home/graviti/下载/scalp_text/result_%06d.txt" $count $count $count);
   count=`expr $count + 1`;
 done
 
