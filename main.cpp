90d03a1933be18b94d1b8cbb2a15a8287b6ad4fe
Tim <16307130090@fudan.edu.cn>
2019-08-29 13:18:52 +0800

    Deleted Image saving

--- main.cpp
+++ main.cpp
@@ -40,64 +40,73 @@ int main(int argc, char** argv)
 
     cv::Mat labels(img.rows,img.cols,CV_32SC1,mbs.GetSuperpixelLabels());
     // cv::watershed(img,labels);
+    std::ofstream out_file;
+    out_file.open(argv[2]);
 
     // Find watershed lines on the image
-    std::vector<int> r_hist(26);
-    std::vector<int> g_hist(26);
-    std::vector<int> b_hist(26);
-
-    for(int i=0;i<img.rows;i++)
+    if(argc>3)
     {
-        for(int j=0;j<img.cols;j++)
+        // find the right color
+        std::vector<int> r_hist(26);
+        std::vector<int> g_hist(26);
+        std::vector<int> b_hist(26);
+
+        for(int i=0;i<img.rows;i++)
         {
-            cv::Vec3b colors = img.at<cv::Vec3b>(i,j);
-            r_hist[colors[0]/10] += 1;
-            g_hist[colors[1]/10] += 1;
-            b_hist[colors[2]/10] += 1;
-        }
-    }
-    
-
-
-    auto smallest = std::min_element(std::begin(r_hist), std::end(r_hist));
-    long rp = std::distance(std::begin(r_hist), smallest) * 10 + 5;
-
-    smallest = std::min_element(std::begin(g_hist), std::end(g_hist));
-    long gp = std::distance(std::begin(g_hist), smallest) * 10 + 5;
-
-    smallest = std::min_element(std::begin(b_hist), std::end(b_hist));
-    long bp = std::distance(std::begin(b_hist), smallest) * 10 + 5;
-
-    cv::Mat aftermath = img.clone();
-    std::ofstream out_file;
-    out_file.open(argv[3]);
-
-    for (int i=1; i<img.rows-1; i++)
-    {
-        for (int j=1; j<img.cols-1; j++)
-        {
-            out_file << labels.at<int>(i,j) << " ";
-            if(labels.at<int>(i,j)!= labels.at<int>(i,j-1) || labels.at<int>(i,j)!= labels.at<int>(i-1,j) || labels.at<int>(i,j)!= labels.at<int>(i+1,j) || labels.at<int>(i,j)!= labels.at<int>(i,j+1))
-
+            for(int j=0;j<img.cols;j++)
             {
-                // is boundry
-                aftermath.at<cv::Vec3b>(i,j)[0] = rp; // 255 - img.at<cv::Vec3b>(i,j)[0];
-                aftermath.at<cv::Vec3b>(i,j)[1] = gp; // 255 - img.at<cv::Vec3b>(i,j)[1];
-                aftermath.at<cv::Vec3b>(i,j)[2] = bp; // 255 - img.at<cv::Vec3b>(i,j)[2];
+                cv::Vec3b colors = img.at<cv::Vec3b>(i,j);
+                r_hist[colors[0]/10] += 1;
+                g_hist[colors[1]/10] += 1;
+                b_hist[colors[2]/10] += 1;
             }
         }
-        out_file << std::endl;
-    }
+
+        auto smallest = std::min_element(std::begin(r_hist), std::end(r_hist));
+        long rp = std::distance(std::begin(r_hist), smallest) * 10 + 5;
+
+        smallest = std::min_element(std::begin(g_hist), std::end(g_hist));
+        long gp = std::distance(std::begin(g_hist), smallest) * 10 + 5;
+
+        smallest = std::min_element(std::begin(b_hist), std::end(b_hist));
+        long bp = std::distance(std::begin(b_hist), smallest) * 10 + 5;
+
+        cv::Mat aftermath = img.clone();
+        
+        for (int i=1; i<img.rows-1; i++)
+        {
+            for (int j=1; j<img.cols-1; j++)
+            {
+                if(labels.at<int>(i,j)!= labels.at<int>(i,j-1) || labels.at<int>(i,j)!= labels.at<int>(i-1,j) || labels.at<int>(i,j)!= labels.at<int>(i+1,j) || labels.at<int>(i,j)!= labels.at<int>(i,j+1))
+                {
+                    // is boundry
+                    aftermath.at<cv::Vec3b>(i,j)[0] = rp; // 255 - img.at<cv::Vec3b>(i,j)[0];
+                    aftermath.at<cv::Vec3b>(i,j)[1] = gp; // 255 - img.at<cv::Vec3b>(i,j)[1];
+                    aftermath.at<cv::Vec3b>(i,j)[2] = bp; // 255 - img.at<cv::Vec3b>(i,j)[2];
+                }
+            }
+        }
     // std::cout << labels << std::endl;
-    std::stringstream ss;
-    // ss << "/home/graviti/下载/scalp_results/";
-    // if(argc>2)
-    ss << argv[2];
-    // else
-    //  ss << "scalp_result";
-    // ss << ".png";
-    std::string filename = ss.str();
-    cv::imwrite(filename, aftermath);
+        std::stringstream ss;
+        ss << argv[3];
+        // else
+        //  ss << "scalp_result";
+        // ss << ".png";
+        std::string filename = ss.str();
+        cv::imwrite(filename, aftermath);
+    }
+    else
+    {
+        for (int i=1; i<img.rows-1; i++)
+        {
+            for (int j=1; j<img.cols-1; j++)
+            {
+                out_file << labels.at<int>(i,j) << " "; // NOTICE!
+            }
+            out_file << std::endl;
+        }
+    }
+
     end = clock();
     double endtime=(double)(end-start)/CLOCKS_PER_SEC;
     std::cout << "Total Time: " << endtime*1000.0 << "ms" << std::endl;
